c3ed3dd9164125bc0bd29963ca44d2776be92c77
Can Burak S <canburak@msn.com>
2021-10-08 23:35:36 +0300

    Initial commit

---
+++ components/Footer.tsx
@@ -0,0 +1,116 @@
+import * as React from 'react'
+import { useRouter } from 'next/router'
+import { FaTwitter, FaGithub, FaLinkedin } from 'react-icons/fa'
+import { IoSunnyOutline, IoMoonSharp } from 'react-icons/io5'
+import * as config from 'lib/config'
+
+import styles from './styles.module.css'
+
+export default function Home() {
+  function makeLinksNoFollow() {
+    const whitelist = [
+      'www.filizguvenlik.com.tr',
+      'livicom.net',
+      'livicomturkiye.com',
+      'www.cbsofyalioglu.com',
+      'alarmsistemleri.org',
+      'tr.wikipedia.org'
+    ]
+
+    const allAnchors = document.querySelectorAll('a')
+    for (const a of allAnchors.values()) {
+      if (!whitelist.includes(a.host)) {
+        a.rel = 'nofollow noopener'
+      }
+    }
+  }
+  React.useEffect(() => {
+    makeLinksNoFollow()
+  }, [])
+  return (
+    <div>
+      {}
+    </div>
+  )
+}
+export const Footer: React.FC<{
+  isDarkMode: boolean
+  toggleDarkMode: () => void
+}> = ({ isDarkMode, toggleDarkMode }) => {
+  //const router = useRouter()
+  //console.log('\n\n', router.pathname)
+  const [hasMounted, setHasMounted] = React.useState(false)
+  const toggleDarkModeCb = React.useCallback(
+    (e) => {
+      e.preventDefault()
+      toggleDarkMode()
+    },
+    [toggleDarkMode]
+  )
+
+  React.useEffect(() => {
+    setHasMounted(true)
+  }, [])
+
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.copyright}>
+        Copyright 2021.{' '}
+        <a href='https://www.cbsofyalioglu.com/' rel='me'>
+          {config.author}
+        </a>
+      </div>
+
+      {hasMounted ? (
+        <div className={styles.settings}>
+          <a
+            className={styles.toggleDarkMode}
+            onClick={toggleDarkModeCb}
+            title='Tottle dark mode'
+          >
+            {isDarkMode ? <IoMoonSharp /> : <IoSunnyOutline />}
+          </a>
+        </div>
+      ) : null}
+
+      <div className={styles.social}>
+        {config.twitter && (
+          <a
+            className={styles.twitter}
+            href={`https://twitter.com/${config.twitter}`}
+            title={`Twitter @${config.twitter}`}
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <FaTwitter />
+          </a>
+        )}
+
+        {config.github && (
+          <a
+            className={styles.github}
+            href={`https://github.com/${config.github}`}
+            title={`GitHub @${config.github}`}
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <FaGithub />
+          </a>
+        )}
+
+        {config.linkedin && (
+          <a
+            className={styles.linkedin}
+            href={`https://www.linkedin.com/in/${config.linkedin}`}
+            title={`LinkedIn ${config.author}`}
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <FaLinkedin />
+          </a>
+        )}
+      </div>
+      <Home />
+    </footer>
+  )
+}
---
+++ site.config.js
@@ -0,0 +1,50 @@
+module.exports = {
+  rootNotionPageId: '219a533a72de47eca2ec8814c488a578',
+
+  // if you want to restrict pages to a single notion workspace (optional)
+  // (this should be a Notion ID; see the docs for how to extract this)
+  rootNotionSpaceId: null,
+
+  // basic site info (required)
+  name: 'Alarm Sistemleri',
+  domain: 'alarmsistemleri.org',
+  author: 'Can Burak Sofyalıoğlu',
+
+  // open graph metadata (optional)
+  description: 'Alarm sistemleri ve güvenlik teknolojileri üzerine yazılar.',
+  socialImageTitle: 'Alarm Sistemleri Blog',
+  socialImageSubtitle: 'Güvenlik ve Alarm Sistemleri Yazıları',
+
+  // social usernames (optional)
+  twitter: 'transitive_bs',
+  github: 'transitive-bullshit',
+  linkedin: 'fisch2',
+
+  // default notion icon and cover images for site-wide consistency (optional)
+  // page-specific values will override these site-wide defaults
+  defaultPageIcon: null,
+  defaultPageCover: null,
+  defaultPageCoverPosition: 0.5,
+
+  // image CDN host to proxy all image requests through (optional)
+  // NOTE: this requires you to set up an external image proxy
+  imageCDNHost: null,
+
+  // Utteranc.es comments via GitHub issue comments (optional)
+  utterancesGitHubRepo: null,
+
+  // whether or not to enable support for LQIP preview images (optional)
+  // NOTE: this requires you to set up Google Firebase and add the environment
+  // variables specified in .env.example
+  isPreviewImageSupportEnabled: false,
+
+  // map of notion page IDs to URL paths (optional)
+  // any pages defined here will override their default URL paths
+  // example:
+  //
+  // pageUrlOverrides: {
+  //   '/foo': '067dd719a912471ea9a3ac10710e7fdf',
+  //   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
+  // }
+  pageUrlOverrides: null
+}
